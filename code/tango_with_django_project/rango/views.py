4c814235c6b714609d70ee33e164ad35d1e33e64
Mike <arcticfeather@hotmail.com>
2015-02-02 10:59:18 +0900

    Set up static and media files.  Added basic models for database

--- code/tango_with_django_project/rango/views.py
+++ code/tango_with_django_project/rango/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse, render
 
 def index(request):
-    return HttpResponse("""Rango says hello world!
-        <br/><a href=\"/rango/about\">About us</a>""")
+    context_dict = { "boldmessage": "I am bold font from the context"}
+
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    return HttpResponse("""Rango says this is the about page.
-        <br/><a href=\"/rango\">Home</a>""")
+    context_dict = {"aboutmessage": "This is the about page."}
+
+    return render(request, 'rango/about.html', context_dict)
--- code/tango_with_django_project/tango_with_django_project/settings.py
+++ code/tango_with_django_project/tango_with_django_project/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
@@ -23,6 +22,12 @@ SECRET_KEY = '-_tqc0=pj6#h24a3d8(toz#uw=zfb@7crt2m2)sp7dws*8h62z'
 DEBUG = True
 
 TEMPLATE_DEBUG = True
+
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+
+TEMPLATE_DIRS = [
+    TEMPLATE_PATH,
+]
 
 ALLOWED_HOSTS = []
 
@@ -81,4 +86,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
