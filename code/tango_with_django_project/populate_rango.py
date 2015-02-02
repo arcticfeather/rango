1b592fa2735ebd3c0b6d132eea9a3dddaa7bb4a8
Mike <arcticfeather@hotmail.com>
2015-02-02 11:39:16 +0900

    Added script to populate database and made small modifications to admin

---
+++ code/tango_with_django_project/populate_rango.py
@@ -0,0 +1,90 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
+import django
+django.setup()
+from rango.models import Category, Page
+
+def populate():
+    python_cat = add_cat(
+        name='Python',
+        views=128,
+        likes=64
+    )
+
+    add_page(
+        cat=python_cat,
+        title="Offical Python Tutorial",
+        url="http://docs.python.org/2/tutorial"
+    )
+
+    add_page(
+        cat=python_cat,
+        title="How To Think Like A Computer Scientist",
+        url="http://www.greenteapress.com/thinkpython/"
+    )
+
+    add_page(
+        cat=python_cat,
+        title="Learn Python In 10 Minutes",
+        url="http://www.korokithakis.net/tutorials/python"
+    )
+
+    django_cat = add_cat(
+        name='Django',
+        views=64,
+        likes=32
+    )
+
+    add_page(
+        cat=django_cat,
+        title="Official Django Tutorial",
+        url="http://docs.djangoproject.com/en/1.7/intro/tutorial01/"
+    )
+
+    add_page(
+        cat=django_cat,
+        title="Django Rocks",
+        url="http://www.djangorocks.com/"
+    )
+
+    add_page(
+        cat=django_cat,
+        title="How To Tango With Django",
+        url="http://www.tangowithdjango.com/"
+    )
+
+    frame_cat = add_cat(
+        name="Other Frameworks",
+        views=32,
+        likes=16
+    )
+
+    add_page(
+        cat=frame_cat,
+        title="Flask",
+        url="http://flask.pocoo.org/"
+    )
+
+    add_page(
+        cat=frame_cat,
+        title="Bottle",
+        url="http://bottlepy.org/docs/dev/"
+    )
+
+    # Print out what we added
+    for c in Category.objects.all():
+        for p in Page.objects.filter(category=c):
+            print "- {0} - {1}".format(str(c), str(p))
+
+def add_page(cat, title, url, views=0):
+    p = Page.objects.get_or_create(category=cat, title=title, url=url, views=views)[0]
+    return p
+
+def add_cat(name, views=0, likes=0):
+    c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
+    return c
+
+# Execute function
+if __name__ == '__main__':
+    print "Starting rango population script..."
+    populate()
--- code/tango_with_django_project/rango/models.py
+++ code/tango_with_django_project/rango/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name_plural = "categories"
 
     def __unicode__(self):
         return self.name
