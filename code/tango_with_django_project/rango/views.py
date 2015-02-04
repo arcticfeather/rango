653f821115934a7768d6299f352aa077f5555189
Mike <arcticfeather@hotmail.com>
2015-02-04 14:21:20 +0900

    Login aninand logout implemented.  Test restricted page and adding new categories and pages now requires login.  User is redirected to login page if they try to access when logged out.

--- code/tango_with_django_project/rango/views.py
+++ code/tango_with_django_project/rango/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import HttpResponse, render
 from rango.models import Category, Page
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.decorators import login_required
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 
 def index(request):
@@ -38,6 +41,7 @@ def about(request):
 
     return render(request, 'rango/about.html', context_dict)
 
+@login_required
 def add_category(request):
     if request.method == 'POST':
         form = CategoryForm(request.POST)
@@ -53,6 +57,7 @@ def add_category(request):
 
     return render(request, 'rango/add_category.html', {'form': form})
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         cat = Category.objects.get(slug=category_name_slug)
@@ -112,3 +117,33 @@ def register(request):
         'rango/register.html',
         {'user_form': user_form, 'profile_form': profile_form, 'registered': registered}
     )
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+
+        # if good username/password combo
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/rango/')
+            else:
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            print "Invalid login details: {0}, {1}".format(username, password)
+            error = "Invalid username or password.  Try again."
+            return render(request, 'rango/login.html', {'error': error})
+    else:
+        return render(request, 'rango/login.html', {})
+
+@login_required
+def restricted(request):
+    return HttpResponse("Since you're logged in, you can see this text!")
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/rango/')
--- code/tango_with_django_project/tango_with_django_project/settings.py
+++ code/tango_with_django_project/tango_with_django_project/settings.py
@@ -96,3 +96,5 @@ STATICFILES_DIRS = (
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_URL = '/rango/login/'
